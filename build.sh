d53566543be4b6ddc936e401863f0310a5ef7eb1
Nathan Benner <nathan.benner2@nutrien.com>
2025-08-29 14:19:56 -0600

    basic structure

---
+++ build.sh
@@ -0,0 +1,12 @@
+container=$(buildah from golang:latest)
+
+buildah config --workingdir /usr/src/app $container
+buildah copy $container go.mod go.sum ./
+buildah run $container -- go mod download
+buildah copy $container . .
+buildah run $container -- go build -v -o /usr/local/bin/starlink-exporter ./...
+
+buildah config --entrypoint starlink-exporter $container
+buildah config --author "Nathan Benner nathan.benner2@nutrien.com" --label name=starlink-exporter $container
+
+buildah commit $container starlink-exporter
